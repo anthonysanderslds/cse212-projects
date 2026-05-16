001732dd724b28de668d760be6d8ebab2b915b18
anthonysanderslds <asanders2@byupathway.edu>
2026-05-16 04:46:21 -0500

    added test cases and fixed errors

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,61 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: enqueue 3 elements with different priorities (A, B, C) and then test that the top priority is dequeued first.
+    // Expected Result: "A"
+    // Defect(s) Found: 1, the RemoveAt function would have failed had I not ordered them.
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("A", 3);
+        priorityQueue.Enqueue("B", 2);
+        priorityQueue.Enqueue("C", 1);
+
+        string topPriority = priorityQueue.Dequeue();
+        Assert.AreEqual("A", topPriority);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Add elements to the queue with the same priority but ensure that the item queued first is dequeued first.
+    // Expected Result: "X"
+    // Defect(s) Found: 1, dequeue wasn't finding the correct priority
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("X", 3);
+        priorityQueue.Enqueue("Y", 3);
+        priorityQueue.Enqueue("Z", 1);
+
+        string firstQueued = priorityQueue.Dequeue();
+        Assert.AreEqual("X", firstQueued);
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Checks for exception handling
+    // Expected Result: InvalidOperationException is thrown with message "The queue is empty."
+    // Defect(s) Found: 0, passed
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+        catch (AssertFailedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(
+                 string.Format("Unexpected exception of type {0} caught: {1}",
+                                e.GetType(), e.Message)
+            );
+        }
+    }
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -43,7 +43,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: 2 defects, this was inserting person at the wrong end of the queue and didn't handle infinite turns (<=0)
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +85,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: 2, same defects as *AddPlayerMidway
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +116,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: 2, Same as previous test just with negative numbers as the turn value vs 0
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
@@ -143,7 +143,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: 0, this passed.
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
